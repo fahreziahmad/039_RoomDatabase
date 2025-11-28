d49d4ddeff7352593053a930f348b587f142975a
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-28 19:22:36 +0700

    Menambahkan rute Home dan Entry pada NavHost beserta aksi navigasinya

--- app/src/main/java/com/example/myroom/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroom/view/uicontroller/PetaNavigasi.kt
@@ -31,3 +31,13 @@ fun HostNavigasi(
         startDestination = DestinasiHome.route,
         modifier = Modifier
     ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
