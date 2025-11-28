48e2a1f14fcb824376383441ef52da521e514c18
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-28 19:19:20 +0700

    Menambahkan interface DestinasiNavigasi sebagai kontrak dasar rute navigasi

---
+++ app/src/main/java/com/example/myroom/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,16 @@
+package com.example.myroom.view.route
+
+
+
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar
+     * halaman.
+     */
+    val titleRes: Int
+}
