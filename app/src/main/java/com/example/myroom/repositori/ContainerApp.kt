72f325a8da71c467ee76ace8757cfe1886b71a8e
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-28 15:50:38 +0700

    add class AplikasiSiswa yang mewarisi Application untuk menginisialisasi ContainerApp pada level aplikasi.

--- app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
@@ -16,3 +16,12 @@ class ContainerDataApp(private val context: Context) : ContainerApp {
     }
 }
 
+class AplikasiSiswa : Application() {
+
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(context = this)
+    }
+}
