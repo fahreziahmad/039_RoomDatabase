081653f821387720b124427c29ff3090abe1d1c4
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-11-28 19:40:53 +0700

    menambahkan komponen FormInputSiswa untuk input data siswa

--- app/src/main/java/com/example/myroom/view/HalamanEntry.kt
+++ app/src/main/java/com/example/myroom/view/HalamanEntry.kt
@@ -95,3 +95,12 @@ fun EntrySiswaBody(
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+) {
