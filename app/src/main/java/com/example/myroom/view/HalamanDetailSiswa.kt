3c325bdb26fd9c14a72bd3cc1063b0609ee27432
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-05 15:56:41 +0700

    tambahkan composable DetailDataSiswa untuk menampilkan informasi siswa dalam Card

--- app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
@@ -127,3 +127,19 @@ private fun BodyDetailDataSiswa(
         }
     }
 }
+
+@Composable
+fun DetailDataSiswa(
+    siswa: Siswa, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
