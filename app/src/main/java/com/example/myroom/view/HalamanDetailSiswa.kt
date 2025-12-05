0db2479c76a7b50c652e45d71d81d3c0f143aace
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-05 15:30:52 +0700

    tambah layout Column dan state konfirmasi hapus pada BodyDetailDataSiswa

--- app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
@@ -98,3 +98,9 @@ private fun BodyDetailDataSiswa(
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
