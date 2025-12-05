47670de426c03dff202fbcbb9364d7eca4e856b9
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-05 15:48:52 +0700

    tambahkan rute DetailSiswaScreen dengan argumen ID siswa

--- app/src/main/java/com/example/myroom/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myroom/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myroom.view.DetailSiswaScreen
 import com.example.myroom.view.EntrySiswaScreen
 import com.example.myroom.view.HomeScreen
+import com.example.myroom.view.route.DestinasiDetailSiswa
+import com.example.myroom.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.myroom.view.route.DestinasiEntry
 import com.example.myroom.view.route.DestinasiHome
 
@@ -38,6 +43,15 @@ fun HostNavigasi(
         }
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+        composable (route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument( name = itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = { navController.navigateUp() }
+            )
         }
     }
 }
