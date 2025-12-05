554e4596643289d33640770373a5277f1474ca85
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-05 15:33:49 +0700

    tambahkan tombol hapus dan dialog konfirmasi pada BodyDetailDataSiswa

--- app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/myroom/view/HalamanDetailSiswa.kt
@@ -108,3 +108,22 @@ private fun BodyDetailDataSiswa(
             siswa = detailSiswaUiState.detailSiswa.toSiswa(),
             modifier = Modifier.fillMaxWidth()
         )
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
